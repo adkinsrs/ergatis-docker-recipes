d30189578684cb78c330188a8e6b08fd490c797d
adkinsrs <shaun.adkins@gmail.com>
2017-02-12 19:02:49 -0500

    filtering questions asked by presenting use-case options

--- lgtseek/setup_container.sh
+++ lgtseek/setup_container.sh
@@ -14,42 +14,85 @@ cp docker-compose.tmpl.yml docker-compose.prod.yml
 
 printf "\nWelcome to the LGTSeek Docker installer. Please follow the prompts below that will help the Docker container access your usable data.\n"
 
+# Get Use-Case information to pare down setup options
+printf "\nFirst, let's figure out which LGTSeek Use Case you wish to employ\n"
+printf "Use Case 1:  Good donor reference and good LGT-free host reference\n"
+printf "Use Case 2:  Good donor reference but unknown host reference\n"
+printf "Use Case 3:  Good host reference but unknown donor reference\n"
+
+PS3="Select a Use Case # (1-3 or 'q' to quit): "
+options("1" "2" "3" "q")
+use_case=''
+select i in "${options[@]}"
+do
+   case $opt in
+        "1")
+            echo "Use case 1 chosen"
+	    use_case=1
+            ;;
+        "2")
+            echo "Use case 2 chosen"
+	    use_case=2
+            ;;
+        "3")
+            echo "Use case 3 chosen"
+	    use_case=3
+            ;;
+        "q")
+            quit_setup
+            ;;
+        *) echo "invalid option... choose again (1-3 or 'q' to quit)."
+	    continue
+	    ;;
+    esac
+done
+
+printf "\nNext it's time to specify where the reference and input files are located\n"
+
 # First ask for location of donor reference directory
-printf  "\nFirst, if you have a donor reference genome or multiple genomes to align against, please specify the directory they are located.  If you wish to skip alignment to a donor, then leave blank.\n"
-printf  "Type 'quit' or 'q' to exit setup.\n[DONOR_DIRECTORY]: "
-read donor_mnt
-if [[ $donor_mnt == 'q' ]] || [[ $donor_mnt == 'quit' ]]; then
-    quit_setup
+if [[ $use_case == '1' ]] || [[ $use_case == '2' ]]; then
+	printf  "\nPlease specify the directory that the donor reference genome(s) is located.\n"
+	printf  "Type 'quit' or 'q' to exit setup.\n[DONOR_DIRECTORY]: "
+	read donor_mnt
+	while [[ -z $donor_mnt ]]; do
+	    printf  "\nThe donor reference genome directory path is required.  Please enter one.\n[DONOR_DIRECTORY]: "
+	    read donor_mnt
+	done
+	if [[ $donor_mnt == 'q' ]] || [[ $donor_mnt == 'quit' ]]; then
+	    quit_setup
+	fi
+	sed -i.bak "s|###DONOR_MNT###|$donor_mnt|" docker-compose.prod.yml
 fi
-if [[ -z $donor_mnt ]]; then
-    donor_mnt='./input_data/donor_ref'
-fi
-sed -i.bak "s|###DONOR_MNT###|$donor_mnt|" docker-compose.prod.yml
 
 # Second ask for location of host reference directory
-printf  "\nSecond, if you have a host reference genome or multiple genomes to align against, please specify the directory they are located.  If you wish to skip alignment to a host, then leave blank.\n"
-printf  "Type 'quit' or 'q' to exit setup.\n[HOST_DIRECTORY]: "
-read host_mnt
-if [[ $host_mnt == 'q' ]] || [[ $host_mnt == 'quit' ]]; then
-    quit_setup
+if [[ $use_case == '1' ]] || [[ $use_case == '3' ]]; then
+	printf  "\nPlease specify the directory that the host reference genome(s) is located.\n"
+	printf  "Type 'quit' or 'q' to exit setup.\n[HOST_DIRECTORY]: "
+	read host_mnt
+	while [[ -z $host_mnt ]]; do
+	    printf  "\nThe host reference genome directory path is required.  Please enter one.\n[HOST_DIRECTORY]: "
+	    read host_mnt
+	done
+	if [[ $host_mnt == 'q' ]] || [[ $host_mnt == 'quit' ]]; then
+	    quit_setup
+	fi
+	sed -i.bak "s|###HOST_MNT###|$host_mnt|" docker-compose.prod.yml
 fi
-if [[ -z $host_mnt ]]; then
-    host_mnt='./input_data/host_ref'
-fi
-sed -i.bak "s|###HOST_MNT###|$host_mnt|" docker-compose.prod.yml
 
 # Third ask for location of Refseq reference directory
-printf  "\nFirst, if you have a RefSeq reference genome or multiple genomes to align against, please specify the directory they are located.  This directory is required.\n"
-printf  "Type 'quit' or 'q' to exit setup.\n[REFSEQ_DIRECTORY]: "
-read refseq_mnt
-while [[ -z $refseq_mnt ]]; do
-    printf  "\nThe RefSeq reference genome directory path is required.  Please enter one.\n[REFSEQ_DIRECTORY]: "
-    read refseq_mnt
-done
-if [[ $refseq_mnt == 'q' ]] || [[ $refseq_mnt == 'quit' ]]; then
-    quit_setup
+if [[ $use_case == '3' ]]; then
+	printf  "\nPlease specify the directory that the RefSeq reference genomes are located.\n"
+	printf  "Type 'quit' or 'q' to exit setup.\n[REFSEQ_DIRECTORY]: "
+	read refseq_mnt
+	while [[ -z $refseq_mnt ]]; do
+	    printf  "\nThe RefSeq reference genome directory path is required.  Please enter one.\n[REFSEQ_DIRECTORY]: "
+	    read refseq_mnt
+	done
+	if [[ $refseq_mnt == 'q' ]] || [[ $refseq_mnt == 'quit' ]]; then
+	    quit_setup
+	fi
+	sed -i.bak "s|###REFSEQ_MNT###|$refseq_mnt|" docker-compose.prod.yml
 fi
-sed -i.bak "s|###REFSEQ_MNT###|$refseq_mnt|" docker-compose.prod.yml
 
 # Next, ask where the output data should be written to
 printf  "\nLastly, what directory should LGTView output be written to?  Note that if you close the Docker container, this output data may disappear, so it is recommended it be copied to a more permanent directory location.  If left blank, the output will be located at './output_data'\n"
@@ -64,15 +107,15 @@ fi
 sed -i.bak "s|###OUTPUT_DATA###|$output_dir|" docker-compose.prod.yml
 
 # Time to determine what Docker host will run the container
-printf  "\nWhat IP is the docker host machine on?  Leave blank if you are using local resources for the host (localhost)\n"
-printf  "Type 'quit' or 'q' to exit setup.\n[RUN_LOCALLY]: "
-read ip_address
-if [[ $ip_address == 'q' ]] || [[ $ip_address == 'quit' ]]; then
-    quit_setup
-fi
-if [[ -z $ip_address ]]; then
+#printf  "\nWhat IP is the docker host machine on?  Leave blank if you are using local resources for the host (localhost)\n"
+#printf  "Type 'quit' or 'q' to exit setup.\n[RUN_LOCALLY]: "
+#read ip_address
+#if [[ $ip_address == 'q' ]] || [[ $ip_address == 'quit' ]]; then
+#    quit_setup
+#fi
+#if [[ -z $ip_address ]]; then
     ip_address='localhost'
-fi
+#fi
 sed -i.bak "s|###IP_HOST###|$ip_address|" docker-compose.prod.yml
 
 
