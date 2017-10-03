fd683f8aba3d129d0e88d4b7ad5db7db96bd2b95
Shaun Adkins <sadkins@som.umaryland.edu>
2017-10-03 15:34:59 -0400

    more changes related to bioconductor

--- rnaseq/install_deseq.sh
+++ rnaseq/install_deseq.sh
@@ -2,20 +2,12 @@
 
 Rver="R-3.3.1"
 
-# Installs to get R working
-#apt-get --force-yes -y update
-#apt-get --force-yes -y install gfortran libbz2-dev libpcre3 libpcre3-dev libcurl4-openssl-dev
+# The "Matrix" package dependency will fail if these aren't symlinked
+ln -s /usr/lib/liblapack.so.3 /usr/lib/liblapack.so
+ln -s /usr/lib/libblas.so.3 /usr/lib/libblas.so
 
-# Install and build R (Using 'apt-get install' on Ubuntu Trusty installs version 3.0.2 of R)
-#curl https://cran.r-project.org/src/base/R-3/${Rver}.tar.gz | tar -C /opt -zx
-#cd /opt/${Rver}
-#/opt/${Rver}/configure --with-readline=no --with-x=no || exit 1
-#make || exit 1
-#make install || exit 1
-
-#apt-get --force-yes -y install r-base-dev
-
-echo "source(\"http://bioconductor.org/biocLite.R\"); 
+echo "install.packages(c(\"survival\"), repos=\"https://lib.stat.cmu.edu/R/CRAN/\")" | R --save --restore
+echo "source(\"http://bioconductor.org/biocLite.R\");
 biocLite(c(\"DESeq\"), ask=FALSE)" | R --save --restore
 
 # done
