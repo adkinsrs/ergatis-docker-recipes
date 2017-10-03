e3dc53cbe536301f63216dbd2af267cf8414a8b9
Shaun Adkins <sadkins@som.umaryland.edu>
2017-10-03 11:00:14 -0400

    removing unnecessary line

--- rnaseq/install_bioc.sh
+++ rnaseq/install_bioc.sh
@@ -2,8 +2,6 @@
 
 Rver="R-3.0.2"
 
-echo "install.packages(repos=\"http://lib.stat.cmu.edu/R/CRAN/\")" | R --save --restore
-
 echo "source(\"http://bioconductor.org/biocLite.R\"); 
 biocLite(c(\"cummeRbund\", \"DESeq\", \"edgeR\"), ask=FALSE)" | R --save --restore
 
